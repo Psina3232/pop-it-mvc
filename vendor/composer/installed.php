122fc8b2ee9465673505d2f1c4928f13f235db1e
zhura <zhuravlevkos9@gmail.com>
2024-03-13 18:37:08 +0700

    настройка автозагрузки классов, обновил composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'psina3232/pop-it-mvc',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'bc8f9d81bfe1d27568029fa083551a8d1afdd9f8',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'psina3232/pop-it-mvc' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'bc8f9d81bfe1d27568029fa083551a8d1afdd9f8',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
